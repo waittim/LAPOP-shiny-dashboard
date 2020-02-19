21dc2418a64910cd46dd8055338c4fffa604b328
Zekun Wang <zekun.wang.us@gmail.com>
2020-02-19 16:41:11 -0600

    server

---
+++ internet.R
@@ -0,0 +1,7 @@
+rsconnect::setAccountInfo(name='zekun',
+                          token='A8DBCC8131BC12F046A181E12B68DA3F',
+                          secret='abancTPYUwPIb6Q/K6swSOf2crKA+M+xYB7Nn5Kf')
+
+
+library(rsconnect)
+rsconnect::deployApp('app.R')
